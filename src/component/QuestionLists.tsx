fb8c6d934b7bd804a31185e85c9851b238920f7e
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-23 22:29:15 +0700

    🆙update (question list): create props

--- src/component/QuestionLists.tsx
+++ src/component/QuestionLists.tsx
@@ -1,24 +1,36 @@
 "use client";
 import React, { useState } from "react";
 
-const QuestionLists = () => {
+interface IProps {
+  color?: string;
+  question: string;
+  answer: string;
+}
+
+const QuestionLists = (props: IProps) => {
   const [openQuestion, setopenQuestion] = useState(false);
   return (
-    <div className="">
-      <div className="bg-red-300 p-4 rounded-sm flex flex-row">
-        <p>
-          Can I use FrontendPro to prepare for a Frontend Developer interview?
-        </p>
-        <button
-          className="ml-4 font-semibold sm:text-xl md:text-2xl"
-          onClick={() => setopenQuestion(!openQuestion)}
+    <>
+      <div className="">
+        <div
+          className={`p-4 rounded-sm flex flex-row ${
+            props.color ? `bg-${props.color}` : ""
+          }`}
         >
-          {openQuestion ? 'x' : '+'}
-        </button>
-      </div>
+          <p>{props.question}</p>
+          <button
+            className="ml-4 font-semibold sm:text-xl md:text-2xl"
+            onClick={() => setopenQuestion(!openQuestion)}
+          >
+            {openQuestion ? "x" : "+"}
+          </button>
+        </div>
 
-      <div>{openQuestion && <div className="bg-red-300 p-4">Sure</div>}</div>
-    </div>
+        <div className={openQuestion ? "" : "hidden"}>
+          <div className={`bg-${props.color} p-4`}>{props.answer}</div>
+        </div>
+      </div>
+    </>
   );
 };
 
