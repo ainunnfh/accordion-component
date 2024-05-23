bfcaac1c6208f336043eb5b20344c78b572380ee
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-23 22:19:23 +0700

    🆙update (question list): create state for open question

--- src/component/QuestionLists.tsx
+++ src/component/QuestionLists.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const QuestionLists = () => {
+  const [openQuestion, setopenQuestion] = useState(false);
   return (
-    <div className="flex items-center">
+    <div className="">
       <div className="bg-red-300 p-4 rounded-sm flex flex-row">
         <p>
           Can I use FrontendPro to prepare for a Frontend Developer interview?
         </p>
-      <button className="ml-4 font-semibold sm:text-xl md:text-2xl ">+</button>
+        <button
+          className="ml-4 font-semibold sm:text-xl md:text-2xl"
+          onClick={() => setopenQuestion(!openQuestion)}
+        >
+          {openQuestion ? 'x' : '+'}
+        </button>
       </div>
 
+      <div>{openQuestion && <div className="bg-red-300 p-4">Sure</div>}</div>
     </div>
   );
 };
