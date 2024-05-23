da3763fcf1027ad690e922adece029d16d1b85c9
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-23 22:13:51 +0700

    ⭐new: create question list component

---
+++ src/component/QuestionLists.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const QuestionLists = () => {
+  return (
+    <div className="flex items-center">
+      <div className="bg-red-300 p-4 rounded-sm flex flex-row">
+        <p>
+          Can I use FrontendPro to prepare for a Frontend Developer interview?
+        </p>
+      <button className="ml-4 font-semibold sm:text-xl md:text-2xl ">+</button>
+      </div>
+
+    </div>
+  );
+};
+
+export default QuestionLists;
