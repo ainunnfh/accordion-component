bdf190d4cfe65733659d1cc8af804379d382e4ba
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-25 20:16:26 +0700

    💄update (card): add color for each question

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -10,11 +10,11 @@ const Card = () => {
       </div>
 
       <div className=''>
-        <QuestionLists question='Can I use FrontendPro to prepare for a Frontend Developer interview?' answer='Sure' color='yellow' />
+        <QuestionLists question='Can I use FrontendPro to prepare for a Frontend Developer interview?' answer='Sure' color='yellow-300' />
         <QuestionLists question='Do I need to have experience in frontend dev to use FrontendPro?' answer='Absolutely no' color='blue-300' />
         <QuestionLists question='How often are new challenges added to FrontendPro?' answer='Every month' color='purple-300' />
         <QuestionLists question='What kind of frontend challenges can I expect to find on FrontendPro?' answer='Beginner, Intermediate and Advanced' color='red-300' />
-        <QuestionLists question='Can I use libraries or frameworks on this projects?' answer='You can explore with framework to speed your work' color='slate-300' />
+        <QuestionLists question='Can I use libraries or frameworks on this projects?' answer='You can explore with framework to speed your work' color='green-300' />
       </div>
     </div>
   )
