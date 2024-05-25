95cc3e39df808ae076d67ad7686afb52cd93d5b0
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-05-25 20:10:20 +0700

    🆙update (card): instances of question lists compt

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -10,7 +10,11 @@ const Card = () => {
       </div>
 
       <div className=''>
-        <QuestionLists question='Can I use FrontendPro to prepare for a Frontend Developer interview?' answer='Sure' color='red-300' />
+        <QuestionLists question='Can I use FrontendPro to prepare for a Frontend Developer interview?' answer='Sure' color='yellow' />
+        <QuestionLists question='Do I need to have experience in frontend dev to use FrontendPro?' answer='Absolutely no' color='blue-300' />
+        <QuestionLists question='How often are new challenges added to FrontendPro?' answer='Every month' color='purple-300' />
+        <QuestionLists question='What kind of frontend challenges can I expect to find on FrontendPro?' answer='Beginner, Intermediate and Advanced' color='red-300' />
+        <QuestionLists question='Can I use libraries or frameworks on this projects?' answer='You can explore with framework to speed your work' color='slate-300' />
       </div>
     </div>
   )
