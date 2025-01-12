f65f3e2822e9084b41b82ac5a9196b553abf0988
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-12 22:45:48 +0700

    💄update card: remove color shade

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,23 +1,47 @@
-import React from 'react'
-import QuestionLists from './QuestionLists'
+import React from "react";
+import QuestionLists from "./QuestionLists";
 
 const Card = () => {
   return (
-    <div className='bg-white rounded-lg shadow-md p-20'>
-      <div className='text-center'>
-        <h1 className='sm: text-xl md:text-2xl font-semibold'>Frequently Asked Question</h1>
-        <h3>Answer to common questions about our frontend challenges website</h3>
+    <div className="bg-white rounded-lg shadow-md p-20">
+      <div className="text-center">
+        <h1 className="sm: text-xl md:text-2xl font-semibold">
+          Frequently Asked Question
+        </h1>
+        <h3>
+          Answer to common questions about our frontend challenges website
+        </h3>
       </div>
 
-      <div className=''>
-        <QuestionLists question='Can I use FrontendPro to prepare for a Frontend Developer interview?' answer='Sure' color='yellow-300' />
-        <QuestionLists question='Do I need to have experience in frontend dev to use FrontendPro?' answer='Absolutely no' color='blue-300' />
-        <QuestionLists question='How often are new challenges added to FrontendPro?' answer='Every month' color='purple-300' />
-        <QuestionLists question='What kind of frontend challenges can I expect to find on FrontendPro?' answer='Beginner, Intermediate and Advanced' color='red-300' />
-        <QuestionLists question='Can I use libraries or frameworks on this projects?' answer='You can explore with framework to speed your work' color='green-300' />
+      <div className="">
+        <QuestionLists
+          question="Can I use FrontendPro to prepare for a Frontend Developer interview?"
+          answer="Sure"
+          color="yellow"
+        />
+        <QuestionLists
+          question="Do I need to have experience in frontend dev to use FrontendPro?"
+          answer="Absolutely no"
+          color="blue"
+        />
+        <QuestionLists
+          question="How often are new challenges added to FrontendPro?"
+          answer="Every month"
+          color="purple"
+        />
+        <QuestionLists
+          question="What kind of frontend challenges can I expect to find on FrontendPro?"
+          answer="Beginner, Intermediate and Advanced"
+          color="red"
+        />
+        <QuestionLists
+          question="Can I use libraries or frameworks on this projects?"
+          answer="You can explore with framework to speed your work"
+          color="green"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
